8a07a08801dd6a05bccc0209f5a4a27c1bcb05d1
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-05-03 14:27:35 -0400

    checked for the secret code then rendered a unicorn onto the screen with cornify

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ const secretCode = 'tacocat';
 window.addEventListener('keyup', (e) => {
     pressed.push(e.key);
     pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if (pressed.join('').includes(secretCode)) {
+        cornify_add();
+    }
 })
